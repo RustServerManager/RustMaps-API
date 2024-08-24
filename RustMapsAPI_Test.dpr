f514d05fde06b58a55190dcfd90b583863bf5cbc
Adriaan Boshoff <16230645+AdriaanBoshoff@users.noreply.github.com>
2024-08-24 21:09:21 +0200

    Refactor for testing

--- RustMapsAPI_Test.dpr
+++ RustMapsAPI_Test.dpr
@@ -9,19 +9,47 @@ uses
   RustMapsAPI in 'API\RustMapsAPI.pas',
   RustMapsAPI.Types in 'API\RustMapsAPI.Types.pas';
 
-procedure Main;
+const
+  API_KEY = '';
+
+procedure RequestMapGen;
 begin
   var api := TRustMapsAPI.Create;
   try
-    api.APIKey := '';
+    api.APIKey := API_KEY;
 
-    var res := api.GetMap('dsfsdfdsfdsfdsf');
-    Writeln(res.Data);
+    var res := api.RequestMapGeneration(3123, 613511321, False);
+    Writeln(res.ParsedData.MapID);
+    Writeln(res.ParsedData.QueuePosition);
+    Writeln(res.ParsedData.State);
     Writeln(res.StatusCode.ToString);
     Writeln(res.StatusText);
+    Writeln(res.RawData);
   finally
     api.Free;
   end;
+end;
+
+procedure GetMapByID;
+begin
+  var api := TRustMapsAPI.Create;
+  try
+    api.APIKey := API_KEY;
+
+    var res := api.GetMap('30e4c14b699a4999a2c3c088a4f0c7b6');
+
+    Writeln(res.StatusCode.ToString);
+    Writeln(res.StatusText);
+    Writeln(res.RawData);
+    Writeln(res.ParsedData.URL);
+  finally
+    api.Free;
+  end;
+end;
+
+procedure Main;
+begin
+  GetMapByID;
 end;
 
 begin
